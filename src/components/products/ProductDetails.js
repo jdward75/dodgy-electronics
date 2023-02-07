68c81415a442ddc852d1282134aa1871601c38f8
Jonathan Ward <jdward75@gmail.com>
2023-02-08 07:04:09 +1100

    Another commit

--- src/components/products/ProductDetails.js
+++ src/components/products/ProductDetails.js
@@ -4,8 +4,13 @@ const ProductDetails = (props) => {
   return (
     <div className={styles["product-details"]}>
       <div className={styles["product-details-content"]}>
-        {props.product.id}
         <button onClick={props.hideProductDetails}>Close</button>
+        <h3>{props.product.title}</h3>
+        <img
+          src={require(`../../assets/${props.product.image}`)}
+          alt={props.product.title}
+        />
+        <h3>{props.product.price}</h3>
       </div>
     </div>
   );
