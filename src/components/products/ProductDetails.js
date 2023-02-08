1c1b66b14cd227f0b652e13894c3334c0461303b
Jonathan Ward <jdward75@gmail.com>
2023-02-09 07:51:33 +1100

    Another commit

--- src/components/products/ProductDetails.js
+++ src/components/products/ProductDetails.js
@@ -1,16 +1,25 @@
 import styles from "./ProductDetails.module.css";
-
+import Input from "../UI/Input";
 const ProductDetails = (props) => {
   return (
-    <div className={styles["product-details"]}>
-      <div className={styles["product-details-content"]}>
-        <button onClick={props.hideProductDetails}>Close</button>
-        <h3>{props.product.title}</h3>
+    <div className={styles["main-container"]}>
+      <div className={styles.content}>
+        <button
+          className={styles["close-button"]}
+          onClick={props.hideProductDetails}
+        >
+          X
+        </button>
+        <h2 className={styles.title}>{props.product.title}</h2>
         <img
+          className={styles.image}
           src={require(`../../assets/${props.product.image}`)}
           alt={props.product.title}
         />
-        <h3>{props.product.price}</h3>
+        <div className={styles.footer}>
+          <h3>{`$${props.product.price}`}</h3>
+          <Input type="number" id="qty" name="qty" label="Update cart qty:" />
+        </div>
       </div>
     </div>
   );
